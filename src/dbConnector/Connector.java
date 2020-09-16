6c4b856c0c112dd373253d9086e9ccfe30955dc7
damienhunter30 <damien.hunter9@gmail.com>
2020-09-16 23:37:39 +0100

    Connector Updated

--- src/dbConnector/Connector.java
+++ src/dbConnector/Connector.java
@@ -1,10 +1,28 @@
 package dbConnector;
 
-public class Connector {
+import java.sql.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Connector{
+	public static void databaseConnector(){
+		try{
+			Class.forName("org.mariadb.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retail_db","root","*");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from customer_id");
+			
+			while(rs.next()){
+				//System.out.println(rs.getArray(0).toString());
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+				break;
+			}
+			con.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
 	}
-
+	
+	public static void main(String[] args){
+		Connector.databaseConnector();
+	}
 }
