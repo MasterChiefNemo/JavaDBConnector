d78c84a99fb8eefa7972c1f99fa5b9a7a65fa59f
damienhunter30 <damien.hunter9@gmail.com>
2020-09-16 23:38:44 +0100

    Connector Updated

--- src/dbConnector/Connector.java
+++ src/dbConnector/Connector.java
@@ -11,7 +11,6 @@ public class Connector{
 			ResultSet rs = stmt.executeQuery("select * from customer_id");
 			
 			while(rs.next()){
-				//System.out.println(rs.getArray(0).toString());
 				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
 				break;
 			}
